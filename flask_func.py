420c43734b6da1839600f17bd3ee20316aaa63b7
Adisasson21 <96379294+Adisasson21@users.noreply.github.com>
2022-06-09 13:22:43 +0300

    Update flask_func.py

--- flask_func.py
+++ flask_func.py
@@ -1,13 +1,11 @@
 from flask import Flask
-from func import func_print
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-
-    return func_print()
+    return "hello world"
 
 
 app.run(host="0.0.0.0", port=5000, debug=True)
